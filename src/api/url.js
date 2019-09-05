78ad34c5f4e9bad031179152144cd1ff898ae61c
Miyanaqy <2247762766@qq.com>
2019-09-05 15:33:45 +0800

    15:33

--- src/api/url.js
+++ src/api/url.js
@@ -3,7 +3,7 @@
 // const serverUrl = "http://localhost/api"
 // const serverUrl = 'http://192.168.0.130:8080';
 // const serverUrl = 'http://192.168.0.111:8080';
-const serverUrl = 'https://isadream.cn';
+const serverUrl = 'http://47.110.154.248';
 // const serverUrl = 'http://localhost:8080'
 
 const getParams = (url) => {
